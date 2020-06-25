246d3ec77a741eec58fcbba03d580583f1304fa6
subhash3008 <subhash.ptps@gmail.com>
2020-06-25 10:53:18 +0530

    added mat-vertical-form

--- src/app/main/nomination-form/nomination-form-content/nomination-form-content.component.ts
+++ src/app/main/nomination-form/nomination-form-content/nomination-form-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { MatStepper } from '@angular/material';
 import swal from 'sweetalert2';
 
 @Component({
@@ -13,9 +14,20 @@ export class NominationFormContentComponent implements OnInit {
   teacherList: string[] = [];
   formSubmitted = false;
 
-  @ViewChild('housePrefect') housePrefectCheckbox: ElementRef;
-  @ViewChild('schoolPrefect') schoolPrefectCheckbox: ElementRef;
+  horizontalStepperStep1: FormGroup;
+  horizontalStepperStep2: FormGroup;
+  horizontalStepperStep3: FormGroup;
+  basicDetailsForm: FormGroup;
+  showBasicDetailsError: boolean;
+  abilitiesForm: FormGroup;
+  visionForm: FormGroup;
+  finalForm: FormGroup;
+
+  // @ViewChild('housePrefect') housePrefectCheckbox: ElementRef;
+  // @ViewChild('schoolPrefect') schoolPrefectCheckbox: ElementRef;
   // @ViewChild('successSwal') successSwal: SwalComponent;
+  @ViewChild('formStepper') formStepper: MatStepper;
+  @ViewChild('scrollTop') scrollTop: ElementRef;
 
   constructor(
     private _fb: FormBuilder,
@@ -24,20 +36,20 @@ export class NominationFormContentComponent implements OnInit {
 
   ngOnInit(): void {
     // this.getTeachersList(); 
-    this.initializeStudentForm();
-    this.studentDetails = {
-      studentName: 'DAKSH',
-      class: 'IV A',
-      house: 'DHEERAJ',
-      cards: {
-        worthy: 1,
-        intellect: 0,
-        stature: 1,
-        emotion: 0
-      },
-      houseIncharge: 'MONIKA CHHABRA',
-      classTeacher: 'TARVEEN KAUR'
-    };
+    this.initializeStudentForms();
+    // this.studentDetails = {
+    //   studentName: 'DAKSH',
+    //   class: 'IV A',
+    //   house: 'DHEERAJ',
+    //   cards: {
+    //     worthy: 1,
+    //     intellect: 0,
+    //     stature: 1,
+    //     emotion: 0
+    //   },
+    //   houseIncharge: 'MONIKA CHHABRA',
+    //   classTeacher: 'TARVEEN KAUR'
+    // };
     this.teacherList = [
       'AJAY RAI',
       'AANAND DUBEY',
@@ -63,8 +75,47 @@ export class NominationFormContentComponent implements OnInit {
   //   );
   // }
 
-  initializeStudentForm() {
-    this.studentForm = this._fb.group({
+  initializeStudentForms() {
+    // this.studentForm = this._fb.group({
+    //   resposibilitiesHeld: ['', Validators.required],
+    //   academicPerformance: this._fb.group({
+    //     total: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
+    //     eleventhMidTermPerc: [null, Validators.required],
+    //     attendancePerc: [null, [Validators.required, Validators.min(0), Validators.max(100)]]
+    //   }),
+    //   failedMidtermOrNot: [null, Validators.required],
+    //   scholasticAchievement: ['', Validators.required],
+    //   sportsAchievement: ['', Validators.required],
+    //   otherAchievement: ['', Validators.required],
+    //   indisciplineActions: ['', Validators.required],
+    //   issue1: this._fb.group({
+    //     title: ['', Validators.required],
+    //     content: ['', [Validators.required, this.maxWordsValidation.bind(this)]]
+    //   }),
+    //   issue2: this._fb.group({
+    //     title: ['', Validators.required],
+    //     content: ['', [Validators.required, this.maxWordsValidation.bind(this)]]
+    //   }),
+    //   issue3: this._fb.group({
+    //     title: ['', Validators.required],
+    //     content: ['', [Validators.required, this.maxWordsValidation.bind(this)]]
+    //   }),
+    //   otherReferee: [null],
+    //   positionApplied: ['', Validators.required]
+    // });
+    this.basicDetailsForm = this._fb.group({
+      positionApplied: ['', Validators.required],
+      studentName: ['DAKSH', Validators.required],
+      class: ['IV A', Validators.required],
+      house: ['DHEERAJ', Validators.required],
+      cards: this._fb.group({
+        worthy: [1, Validators.required],
+        intellect: [3, Validators.required],
+        stature: [1, Validators.required],
+        emotion: [0, Validators.required]
+      })
+    });
+    this.abilitiesForm = this._fb.group({
       resposibilitiesHeld: ['', Validators.required],
       academicPerformance: this._fb.group({
         total: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
@@ -76,6 +127,8 @@ export class NominationFormContentComponent implements OnInit {
       sportsAchievement: ['', Validators.required],
       otherAchievement: ['', Validators.required],
       indisciplineActions: ['', Validators.required],
+    });
+    this.visionForm = this._fb.group({
       issue1: this._fb.group({
         title: ['', Validators.required],
         content: ['', [Validators.required, this.maxWordsValidation.bind(this)]]
@@ -88,10 +141,15 @@ export class NominationFormContentComponent implements OnInit {
         title: ['', Validators.required],
         content: ['', [Validators.required, this.maxWordsValidation.bind(this)]]
       }),
-      otherReferee: [null],
-      positionApplied: ['', Validators.required]
+      otherReferee: [null]
     });
-    console.log('INITIALIZED : ', this.studentForm);
+    this.finalForm = this._fb.group({
+      houseIncharge: ['MONIKA CHHABRA'],
+      classTeacher: ['TARVEEN KAUR'],
+      otherReferee: [null]
+    });
+    this.showBasicDetailsError = false;
+    console.log('INITIALIZED : ', this.basicDetailsForm.value, this.abilitiesForm.value, this.visionForm.value, this.finalForm.value);
   }
 
   maxWordsValidation(control: AbstractControl) {
@@ -103,19 +161,21 @@ export class NominationFormContentComponent implements OnInit {
     return null;
   }
 
-  postSelectionChange(e: any) {
-    if (e.target.value === 'schoolPrefect') {
-      this.housePrefectCheckbox.nativeElement.checked = false;
-      this.studentForm.controls.positionApplied.setValue('schoolPrefect');
-    } else if (e.target.value === 'housePrefect') {
-      this.schoolPrefectCheckbox.nativeElement.checked = false;
-      this.studentForm.controls.positionApplied.setValue('housePrefect');
-    }
-  }
+  // postSelectionChange(e: any) {
+  //   if (e.target.value === 'schoolPrefect') {
+  //     this.housePrefectCheckbox.nativeElement.checked = false;
+  //     this.studentForm.controls.positionApplied.setValue('schoolPrefect');
+  //   } else if (e.target.value === 'housePrefect') {
+  //     this.schoolPrefectCheckbox.nativeElement.checked = false;
+  //     this.studentForm.controls.positionApplied.setValue('housePrefect');
+  //   }
+  // }
 
   onSubmit() {
-    console.log('STUDENT FORM VALUE : ', this.studentForm.value);
+    // console.log('STUDENT FORM VALUE : ', this.studentForm.value);
     // this.successSwal.fire();
+
+    console.log('FINALIZED : ', this.basicDetailsForm.value, this.abilitiesForm.value, this.visionForm.value, this.finalForm.value);
     swal.fire({
       title: "Form Submitted",
       text: "Your nomination form has been submitted",
@@ -125,11 +185,32 @@ export class NominationFormContentComponent implements OnInit {
 
   clearStudentForm() {
     this.formSubmitted = true;
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
+    this.basicDetailsForm.reset();
+    this.abilitiesForm.reset();
+    this.visionForm.reset();
+    this.finalForm.reset();
+    this.initializeStudentForms();
+    // window.scrollTo({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth'
+    // });
+    this.formStepper.selectedIndex = 0;
+  }
+
+  onBasicDetailsFormNext() {
+    // console.log('BASIC DETAILS FORM : ', this.basicDetailsForm);
+    this.showBasicDetailsError = true;
+  }
+
+  onFormChange() {
+    // window.scrollTo({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth'
+    // });
+    this.scrollTop.nativeElement.scrollIntoView();
+    // console.log('SCROLLING TO TOP');
   }
 
 }
--- src/app/main/nomination-form/nomination-form.module.ts
+++ src/app/main/nomination-form/nomination-form.module.ts
@@ -6,6 +6,15 @@ import { TranslateModule } from '@ngx-translate/core';
 import { FuseSharedModule } from '@fuse/shared.module';
 import { NominationFormHeaderComponent } from './nomination-form-header/nomination-form-header.component';
 import { NominationFormContentComponent } from './nomination-form-content/nomination-form-content.component';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatStepperModule,
+  MatRadioModule
+} from '@angular/material';
 
 const routes = [
   {
@@ -15,10 +24,20 @@ const routes = [
 ];
 
 @NgModule({
-  declarations: [NominationFormComponent, NominationFormHeaderComponent, NominationFormContentComponent],
-  
+  declarations: [
+    NominationFormComponent,
+    NominationFormHeaderComponent,
+    NominationFormContentComponent
+  ],
   imports: [
     RouterModule.forChild(routes),
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatStepperModule,
     TranslateModule,
     FuseSharedModule
   ],
